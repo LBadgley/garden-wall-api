95ff0301446bfd510305fe612c6ecf5a73b6da09
Laura Badgley <l.badgleyy@gmail.com>
2019-12-12 18:10:13 -0800

    trying to connect to back end

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-/* eslint-disable no-console */ 
+/* eslint-disable no-console */
 require('dotenv').config();
-require('./lib/utils/connect')();
+require('./lib/utils/connect');
+// const bodyParser = require('body-parser');
+
 const app =  require('./lib/app');
 
 const PORT = process.env.PORT || 7890;
---
+++ tests/routes/characters.test.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const request = require('supertest');
+const app = require('../../lib/app');
+const mongoose = require('mongoose');
+const seedData = require('../seedData');
+
+describe('characters routes', () => {
+  beforeAll(() => {
+    return mongoose.connect(process.env.MONGODB_URI, { useNewUrlParse: true })
+      .then(() => seedData());
+  });
+
+  afterAll(() => {
+    return mongoose.connection.dropDatabase()
+      .then(() => mongoose.connection.close());
+  });
+
+  it('can get a list of all characters', () => {
+    return request(app)
+      .get('/api/v1/characters')
+  })
+});
+
+
